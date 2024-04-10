5498e66f537f44e204043548d94ec8150d6a8adb
Gustavo Longon Sobral <105596795+gstlongon@users.noreply.github.com>
2024-04-09 22:23:47 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import {ref} from 'vue'
+
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+    <router-link class="navbar-brand" to="/">Pokedex</router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <router-link class="nav-link active" to="/" aria-current="page">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link active" to="/about" aria-current="page">Sobre nós</router-link>
+        </li>
+      </ul>
+    </div>
+   </div>
+  </nav>
+
+  <div class="main">
+    <RouterView/>
+  </div>
+  <footer class="bg-dark text-light">
+    <div class="">
+      Unimar | ADS | {{ new Date().getFullYear() }}
+    </div>
+  </footer>
+</template>
+<style>
+
+footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
