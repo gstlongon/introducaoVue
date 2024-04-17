141fb87ff419764e1a239731454d3da314bd92ef
Gustavo Longon Sobral <105596795+gstlongon@users.noreply.github.com>
2024-04-16 22:33:25 -0300

    Add files via upload

---
+++ src/components/ListPokemon.vue
@@ -0,0 +1,17 @@
+<script setup>
+    const pokemon = defineProps(["name", "url"])
+    const idPokemon = pokemon.url.split('/')[6]
+    const imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${idPokemon}.png`
+    console.log(idPokemon)
+    
+</script>
+    
+<template>
+    <div class="col-md-4">
+        <div class="card mb-3">
+            <p class="text-center"> {{ pokemon.name }}</p>
+            <img  src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/024.png" alt=".." class="card-img-top">
+            
+        </div>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,54 @@
 <script setup>
+//props - onMounted, reactive, ref
+import { onMounted, reactive, ref } from 'vue';
+import ListPokemon from '../components/ListPokemon.vue'
+
+let pokemons = reactive(ref())
+
+onMounted(() => {
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
+  .then(response => response.json())
+  .then(response => {
+    pokemons.value = response.results
+    console.log(pokemons)
+  })
+})
+
 </script>
 
 <template>
   <main>
-    <h1>Home</h1>
+    <div class="container">
+      <div class="row mt-4">
+        <div class="col-12 col-md-6">
+          <div class="row">
+            <div class="card" style="width: 18rem;">
+              <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png" alt=".." class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">
+                  Pikachu
+                </h5>
+                <p class="card-text">
+                  
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6">
+          <div class="card">
+            <div class="card-body row">
+              <ListPokemon 
+              v-for="pokemon in pokemons"
+              :key="pokemon.name"
+              :name="pokemon.name"
+              :url="pokemon.url"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
   </main>
 </template>
